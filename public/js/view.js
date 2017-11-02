029ee8d784308aaf8273db08fd341011bcd12854
Emily Lau <emilylau5@gmail.com>
2017-11-02 14:32:23 -0700

    auto-complete for city search

--- public/js/view.js
+++ public/js/view.js
@@ -17,8 +17,34 @@ $(document).ready(function() {
       };
       $.post("api/users", addUser);
     }
-    else {
-      alert("Passwords do not match")
-    }
+    console.log(User.password)
   };
 })
+
+jQuery(function () 
+ {
+   jQuery("#f_elem_city").autocomplete({
+    source: function (request, response) {
+     jQuery.getJSON(
+      "http://gd.geobytes.com/AutoCompleteCity?callback=?&q="+request.term,
+      function (data) {
+       response(data);
+      }
+     );
+    },
+    minLength: 3,
+    select: function (event, ui) {
+     var selectedObj = ui.item;
+     jQuery("#f_elem_city").val(selectedObj.value);
+    getcitydetails(selectedObj.value);
+     return false;
+    },
+    open: function () {
+     jQuery(this).removeClass("ui-corner-all").addClass("ui-corner-top");
+    },
+    close: function () {
+     jQuery(this).removeClass("ui-corner-top").addClass("ui-corner-all");
+    }
+   });
+   jQuery("#f_elem_city").autocomplete("option", "delay", 100);
+  });
