b6e853fdffab859ccecc194674db5f91aa8ca909
Emily Lau <emilylau5@gmail.com>
2017-11-02 11:04:28 -0700

    validators for models2

--- models/reviews.js
+++ models/reviews.js
@@ -1,8 +1,8 @@
 module.exports = function(sequelize, DataTypes) {
   var Review = sequelize.define("Review", {
-    hotelId : DataTypes.INTEGER,
-    userId : DataTypes.INTEGER,
-    comment : DataTypes.STRING
+    hotelId: DataTypes.INTEGER,
+    userId: DataTypes.INTEGER,
+    comment: DataTypes.STRING
   });
 
   return Review;
