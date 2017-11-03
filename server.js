470f1aa4cfe5a5402796c8f524bfc22f9135489a
jwong1219 <jwong1219@gmail.com>
2017-11-03 13:15:40 -0700

    updating with working communication of city from autofill object from client to server, and server properly executing google places api call and sending the data back to the client

--- server.js
+++ server.js
@@ -32,3 +32,38 @@ db.sequelize.sync({ force: true }).then(function() {
 app.get("/", function(req, res) {
   res.render("index");
 });
+
+app.post("/search", function(request, response) {//this is Justin's testing of google APIs
+  console.log(request.body);
+
+  var http = require("http");
+  var Client = require('node-rest-client').Client;
+   
+  var client = new Client();
+
+  var gMapsKey = 'AIzaSyD6aSsAsp9fRnCVn31vt9559Qz6S6dqYrM';
+
+  var location = request.body.location.lat + "," + request.body.location.lng;
+  var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?";
+  queryURL += "query=hotel&key="
+  queryURL += gMapsKey;
+  queryURL += "&type=lodging&location=" + location;
+  queryURL += "&radius=5000"
+
+  client.get(queryURL, function(data) {
+    console.log(data.results[0]);
+    // var modData = function(data) {
+      // for(index in data.results) {
+      //   var geoCodeURL = "https://maps.googleapis.com/maps/api/geocode/json?place_id=";
+      //   geoCodeURL += toString(data.results[index].id);
+      //   geoCodeURL += "&key=" + gMapsKey;
+      //   client.get(geoCodeURL, function(response) {
+      //     console.log({geoCodeURL});
+      //     console.log(response);
+      //   })
+      // }
+    // }
+    response.json(data);
+  })
+
+})
