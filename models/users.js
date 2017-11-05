5807b526b86ef4bc90b7c24eceff92bdee651303
Winfred Sunga <winfredsunga01@gmail.com>
2017-11-04 21:36:53 -0700

    added background image and styling on panels

--- models/users.js
+++ models/users.js
@@ -1,5 +1,7 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
+    firstName :  DataTypes.STRING,
+    lastName :  DataTypes.STRING,
     userName : DataTypes.STRING,
     password : DataTypes.STRING,
     email : {
