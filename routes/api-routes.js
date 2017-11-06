0f891eaa265e5bc2702962960c63fd5f287c4df9
Winfred Sunga <winfredsunga01@gmail.com>
2017-11-06 13:06:10 -0800

    add get listener for existing user login

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,6 +9,20 @@ module.exports = function(app) {
     res.render("index");
   });
 
+  app.get("/api/users", function(req, res) {
+    //run a query against the database for the username or password provided
+    db.User.findOne({
+      where : {
+        $or : {
+          userName : req.body.userName,
+          password : req.body.password
+        }
+      }
+    }).then(function(dbUser) {
+      res.json(dbUser);
+    });
+  });
+
   app.post("/api/users", function(req, res) {
     console.log(req.body);
     //go through a series of account info validations and send the response back to client if any issue(s)
@@ -38,19 +52,26 @@ module.exports = function(app) {
       //else if there is no data in the db
       else if (!dbUser) {
         console.log("new user added to account!");
+        // var hashPassword = passHash(req.body.password);
+        // console.log("this is hash 2" + hashPassword)
         //go ahead and insert new account into database
-        db.User.create({
-        firstName : req.body.firstName,
-        lastName : req.body.lastName,
-        email: req.body.email,
-        userName: req.body.userName,
-        password: req.body.password
-      }).then(function(result){
-        //just send the same response as in the above if in order for the client-side validation logic to work
-        res.json({
-          outcome : "success"
+        var hashPassword;
+         bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+          // Store hash in your password DB.
+          console.log("this the hash" + hash)
+          db.User.create({
+          firstName : req.body.firstName,
+          lastName : req.body.lastName,
+          email: req.body.email,
+          userName: req.body.userName,
+          password: hash
+        }).then(function(result){
+          //just send the same response as in the above if in order for the client-side validation logic to work
+          res.json({
+            outcome : "success"
+          });
         });
-      });
+        });
       } 
       //else it means there is data, and need to respond with an object
       else if (dbUser) {
