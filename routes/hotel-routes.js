4e3ace1c7e432a3747a585162b5c87239fcaf340
jwong1219 <jwong1219@gmail.com>
2017-11-07 01:03:36 -0800

    got router set up, commented code still needs to be removed. also fixed some validation issues with hotels with no ratings

---
+++ routes/hotel-routes.js
@@ -0,0 +1,6 @@
+var db = require("../models");
+var express = require("express");
+
+var router = express.Router();
+
+module.exports = router;
---
+++ routes/search-routes.js
@@ -0,0 +1,66 @@
+var db = require("../models");
+var express = require("express");
+
+var router = express.Router();
+
+//this router is on the root "/search"
+router.get("/", function(req, res) {
+  res.render("index");
+});
+
+router.post("/:userid", function(request, response) {//this is Justin's testing of google APIs
+  console.log(request.body);
+  console.log(request.params);
+  var http = require("http");
+  var Client = require('node-rest-client').Client;
+  var spotcrime = require('spotcrime');
+   
+  var client = new Client();
+
+  var gMapsKey = 'AIzaSyDS0mO9a53OQPEB6J4al4DoyH2FlInfx40';
+
+  var location = request.body.location.lat + "," + request.body.location.lng;
+  var queryURL = "https://maps.googleapis.com/maps/api/place/textsearch/json?";
+  queryURL += "query=hotel&key="
+  queryURL += gMapsKey;
+  queryURL += "&type=lodging&location=" + location;
+  queryURL += "&radius=5000"
+
+  db.Search.create({
+    city: request.body.name,
+    startDate: request.body.start,
+    endDate: request.body.end,
+    queryString: queryURL,
+    UserId: parseInt(request.params.userid)
+
+  }).then(function(result) {
+    console.log({result});
+    var hotelsData = {};
+    client.get(queryURL, function(data) {
+
+      var responseData = {
+        hotelsData: data,
+      }
+
+      var crimeLoc = {
+        lat: parseFloat(request.body.location.lat),
+        lon: parseFloat(request.body.location.lng)
+      }
+
+      spotcrime.getCrimes(crimeLoc, .1, function(err, crimes){
+        if(err) {
+          throw err;
+          console.log("error getting crime data");
+        }
+        responseData.crimeData = crimes;
+        response.json(responseData);
+      });
+    })
+  })
+})
+
+router.delete("/:searchid", function(request, response) {
+
+})
+
+module.exports = router;
